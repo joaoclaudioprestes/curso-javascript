80e912544359bdba83e35e347dfad9f123387717
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-04-08 19:09:20 -0300

    atualização

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 100) 
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++ //seria c +1 = 2 <= 6... ou seja ele vai fazer até o 6.
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
